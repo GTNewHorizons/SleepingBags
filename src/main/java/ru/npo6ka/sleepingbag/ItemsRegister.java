11051b2b7a46a15c9386f55a9443324a01fe65e9
boubou_19 <miisterunknown@gmail.com>
2021-12-23 23:03:35 +0100

    removed recipe in the mod, see NHCoreMod for the recipes

--- src/main/java/ru/npo6ka/sleepingbag/ItemsRegister.java
+++ src/main/java/ru/npo6ka/sleepingbag/ItemsRegister.java
@@ -15,7 +15,6 @@ public final class ItemsRegister
     
     public static void init() {
         GameRegistry.registerItem(ItemsRegister.sleepingBagItem = new ItemSleepingBag().setUnlocalizedName("sleepingBag").setCreativeTab(CreativeTabs.tabDecorations).setTextureName("sleepingbag:sleeping_bag").setMaxStackSize(1), "sleepingBag");
-        GameRegistry.addRecipe(new ItemStack(ItemsRegister.sleepingBagItem), new Object[] { "   ", "###", "ccc", '#', new ItemStack(Blocks.wool, 1, 32767), 'c', new ItemStack(Blocks.carpet, 1, 32767) });
         GameRegistry.registerBlock((Block)(ItemsRegister.sleepingBagBlock = (BlockSleepingBag)new BlockSleepingBag().setHardness(0.2f).setBlockName("sleepingBagBlock").setBlockTextureName("sleepingbag:sleeping_bag")), "sleepingBagBlock");
     }
 }
